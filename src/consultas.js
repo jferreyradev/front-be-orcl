788e5caf333461cdc5bfb2e29352fd04453e376c
Constanza <constanzamca@gmail.com>
2025-08-18 12:37:12 -0300

    con error

--- src/consultas.js
+++ src/consultas.js
@@ -31,20 +31,23 @@ order by o.orden, ec.fechahora, ED.ORDENEJEC`,
 export const consultasControl = {  
   ctrlIncDoc: {
     nombre: "Procesos ejecutados",
-    url: "http://localhost:9000/consulta",
+    url: "http://10.6.150.91:9000/consulta",
     method: "POST",
     descripcion: "Lista de procesos ejecutados",
-    sql: `SELECT ed.ID_PROC_DET,
-       PD.DESCRIPCION as DESC_PROC_DET,
-       ed.ORDENEJEC, ed.ESTADO, ec.GRUPOREP
-from WORKFLOW.EJEC_DET ed
-inner join WORKFLOW.EJEC_CAB ec on EC.ID_EJEC_CAB = ED.ID_EJEC_CAB
-inner join WORKFLOW.PROC_CAB pc on PC.ID_PROC_CAB = EC.ID_PROC_CAB
-inner join WORKFLOW.PROC_DET pd on PD.ID_PROC_DET = ED.ID_PROC_DET
-inner join WORKFLOW.PROC_CAB_ORDEN o on O.ID_PROC_CAB = PC.ID_PROC_CAB
-where periodo = usuario.f_activo
-and EC.IDTIPOLIQ = 1
-order by o.orden, ec.fechahora, ED.ORDENEJEC`,
+    sql: ` select c.idrep, l.idtipoliquidacion, l.idgrupo, con.codigo,sum(li.IMP) IMPORTE, count(li.imp) CANTIDAD
+    from usuario.cargo c
+    inner join usuario.liquidacion l on l.idocupcarg = c.idocupcarg
+    inner join usuario.liquidacionitem li on li.idliq = l.idliq
+    inner join usuario.concepto con on con.idconcepto = li.idconcepto and con.codigo = 0
+    where l.idtipoliquidacion in (6,8)
+    and l.idgrupo in (1, 2 ,3)
+    and c.idestadocargo = 1 and c.fechabaja is null
+    --and periodo = to_date('01/02/2013','dd/mm/yyyy')
+    and l.FECHAEMISION = usuario.f_activo
+    and l.idtipoliquidacion = c.idtipoliquidacion
+    and c.idrep in (18,21,25,26,27)
+    group by rollup((l.idtipoliquidacion,  l.idgrupo), (c.idrep,con.codigo))
+    order by l.idtipoliquidacion,l.idgrupo`,
   },
   tipoliq: {
     nombre: "Liquidaciones",
