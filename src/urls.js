c89aaebf27c4e59a02eea8139440b47155c00432
José R. Ferreyra <jferreyra.dev@gmail.com>
2025-08-19 16:06:18 -0300

    uso local y remoto

--- src/urls.js
+++ src/urls.js
@@ -2,5 +2,5 @@
 export const apiUrls = [
   { name: 'Producción', url: 'http://10.6.150.91:9000/' },
   { name: 'Desarrollo', url: 'https://api2.com/' },
-  { name: 'Test', url: 'https://api3.com/' }
+  { name: 'local', url: 'http://localhost:9000/' }
 ];
